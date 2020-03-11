1ef7a96671b66fede84c7fd676477eaa43db71cb
j-szulc <james216@wp.pl>
2020-03-11 04:55:41 +0100

    Moved stuff that's both I/O to output.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from evdev import ecodes, InputDevice, list_devices
-from select import select
-from evdev.uinput import UInput
 
-def select_device():
-    """Select a device from the list of accessible input devices."""
 
-    devices = [InputDevice(device_fn) for device_fn in reversed(list_devices())]
-    if not devices:
-        print('error: no input devices found (do you have rw permission on /dev/input/*?)')
-        exit(1)
-
-    device_format = '{0:<3} {1.fn:<20} {1.name:<35} {1.phys}'
-    device_lines = [device_format.format(n, d) for n, d in enumerate(devices)]
-
-    print('ID  {:<20} {:<35} {}'.format('Device', 'Name', 'Phys'))
-    print('-' * len(max(device_lines, key=len)))
-    print('\n'.join(device_lines))
-    print('')
-
-    choice = input('Select device [0-{}]:'.format(len(device_lines) - 1))
-    return devices[int(choice)]
+#from events import Action, PKeyEvent, PComboEvent
+#from keys import codeToPKey
+#from combos import PCombo
+#from modifiers import currentModifiers, updateModifiers
+#from hooks import MAP
+from output import *
+from events import *
+from keys import * 
+from combos import *
+from modifiers import *
+from hooks import *
 
 
 
-uinput = UInput()
-device = select_device()
 try:
 	device.grab()
 except IOError:
@@ -36,5 +25,15 @@ try:
 		for event in device.read():
 			if event.type == ecodes.EV_KEY:
 				print(event.code,event.type)
+				action = Action[event.type]
+				pkey = codeToPKey[event.code]
+				pcombo = PCombo(pkey, currentModifiers)
+				
+				pkeyevent = PKeyEvent(pkey, action)				
+				pcomboevent = PComboEvent(pcombo, action)
+
+				for e in [pkeyevent, pcomboevent]:
+					MAP[e].execute()
+
 finally:
 	device.ungrab()
