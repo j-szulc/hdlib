faafe5d74a6c16636d3a26d97c92d9b47559d53d
j-szulc <james216@wp.pl>
2020-03-11 04:03:46 +0100

    Changed map to key_dependent_dict

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 from operator import add
 from itertools import product as itertools_product
+from collections import defaultdict
 # Contains useful code snippets
 
 def fold(list_, fun, start):
@@ -14,3 +15,14 @@ def sum_(list_, start=0):
 # e.g. product([["a","b"],[1,2]]) == [('a', 1), ('a', 2), ('b', 1), ('b', 2)]
 def product(list_of_lists):
 	return list(itertools_product(*list_of_lists))
+
+
+class key_dependent_dict(defaultdict):
+
+	def __init__(self,f_of_x):
+		super().__init__(None) # base class doesn't get a factory
+		self.f_of_x = f_of_x # save f(x)
+	def __missing__(self, key): # called when a default needed
+		ret = self.f_of_x(key) # calculate default value
+		self[key] = ret # and install it in the dict
+		return ret
--- hooks.py
+++ hooks.py
@@ -1,10 +1,18 @@
-from collections import defaultdict
+from helpers import key_dependent_dict
 
 class WhatToDo:
+	
+	event = None
 
 	listeners = []
 	capturer = None
 
+	#Function to execute if empty()
+	def fallback(self):
+		pass
+
+	def __init__	
+	
 	def listen(self, func):
 		if(self.capturer == None):
 			self.listeners.append(func)
@@ -12,7 +20,18 @@ class WhatToDo:
 	def capture(self, func):
 		self.capturer = func
 	
-MAP = defaultdict(lambda: WhatToDo())
+	def empty():
+		return self.listeners == [] and self.capturer == None
+
+	def execute(self):
+		if self.empty():
+			self.fallback()
+		for l in self.listeners:
+			l()
+		if(self.capturer != None):
+			self.capturer()
+
+MAP = key_dependent_dict(lambda event: WhatToDo(event))
 
 
 def captureEvent(event):
