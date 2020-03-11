ee9e8b46d5e95c991f46c753fa4fb5044f4c5da9
j-szulc <james216@wp.pl>
2020-03-11 14:52:30 +0100

    Added new errors

--- errors.py
+++ errors.py
@@ -9,3 +9,13 @@ class InvalidKeyStr(Exception):
 	def __init__(self,str_):
 		self.str_=str_
 
+class InvalidKeyCode(Exception):
+
+	def __init__(self,int_):
+		self.int_=int_
+
+class InvalidSth(Exception):
+
+	def __init__(self,sth):
+		self.sth=sth
+		self.type=type(sth)
