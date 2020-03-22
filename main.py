a55c531170f4c0e483862cc717da09e15b6029f4
j-szulc <james216@wp.pl>
2020-03-22 23:14:26 +0100

    Added a simple privilige control

--- main.py
+++ main.py
@@ -8,36 +8,56 @@ from structs.modifiers import *
 from structs.actions import *
 from hooks import *
 
-MAP = Map()
+import os, pwd, grp
 
-listen = MAP.listenCombo
-capture = MAP.captureCombo
-listenKey = MAP.listenKey
-captureKey = MAP.captureKey
+def drop_privileges(uid_name='nobody', gid_name='nobody'):
+    if os.getuid() != 0:
+        # We're not root so, like, whatever dude
+        return
 
-modifierList = ["shift"]
-MODIFIERS = ModifierSet(modifierList)
-for m in modifierList:
-	listenKey(m,Action.PRESS)(MODIFIERS.update)
-	listenKey(m,Action.RELEASE)(MODIFIERS.update)
+    # Get the uid/gid from the name
+    running_uid = pwd.getpwnam(uid_name).pw_uid
+    running_gid = grp.getgrnam(gid_name).gr_gid
 
-@listen("shift-a")
-def f(e):
-	print("Hello")
+    # Remove group privileges
+    os.setgroups([])
 
+    # Try setting the new uid/gid
+    os.setgid(running_gid)
+    os.setuid(running_uid)
 
-def handlingFun(key, action):
-	
-	combo = Combo(key, MODIFIERS.current)
+    # Ensure a very conservative umask
+    old_umask = os.umask(77)
 
-	keyevent = KeyEvent(key, action)				
-	comboevent = ComboEvent(combo, action)
+if(__name__ == "__main__"):
 
-	for e in [keyevent, comboevent]:
-		MAP.execute(e)
+	MAP = Map()
 
-loop(handlingFun = handlingFun, nOfIterations=100)
+	listen = MAP.listenCombo
+	capture = MAP.captureCombo
+	listenKey = MAP.listenKey
+	captureKey = MAP.captureKey
 
-for m in modifierList:
-	Key.fromSth(m).send(Action.RELEASE)
+	modifierList = ["shift"]
+	MODIFIERS = ModifierSet(modifierList)
+	for m in modifierList:
+		listenKey(m,Action.PRESS)(MODIFIERS.update)
+		listenKey(m,Action.RELEASE)(MODIFIERS.update)
+
+	def handlingFun(key, action):
+
+		drop_privileges()
+
+		combo = Combo(key, MODIFIERS.current)
+
+		keyevent = KeyEvent(key, action)				
+		comboevent = ComboEvent(combo, action)
+
+		for e in [keyevent, comboevent]:
+			MAP.execute(e)
+
+	loop(handlingFun = handlingFun, nOfIterations=100)
+
+	for m in modifierList:
+		Key.fromSth(m).send(Action.RELEASE)
 
